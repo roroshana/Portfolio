b78d73fac447010aa0816acb19215ee18b466040
roroshana <100582157+roroshana@users.noreply.github.com>
2026-02-21 04:21:02 -0800

    feat: Implement scroll-to-top on route change
    Adds a `ScrollToTop` component that automatically scrolls the user to the top of the page whenever the route changes. This improves navigation experience by ensuring users see the beginning of new content.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import CaseStudy from "./pages/CaseStudy";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
